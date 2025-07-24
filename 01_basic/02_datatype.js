73fd05cc45a968e8237bb0ba3df89ea652ff8ba8
Rajendra Singh Adhikari <60564843+rajendra1090@users.noreply.github.com>
2025-07-24 17:04:50 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,16 @@
+//Premitive Data Type
+//number 2 to power 53
+//bigint 
+//boolean= True/False
+//string=""
+//null = Standalone value
+//undefind
+//symbol= unquie
+
+// Non Premitive type
+//object
+
+//console.log(typeof "Rajendra"); //String
+//console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
