6eb08eb094ab91f0d2b49a3c275f4815acd3bd4d
Rajendra Singh Adhikari <60564843+rajendra1090@users.noreply.github.com>
2025-07-25 17:31:39 +0000

    datatype update

--- 01_basic/02_datatype.js
+++ 01_basic/02_datatype.js
@@ -1,16 +1,40 @@
 //Premitive Data Type
-//number 2 to power 53
-//bigint 
+//number  till 2 to power 53
+//bigint   
+//var bigInt= 12343413412341234n
 //boolean= True/False
 //string=""
 //null = Standalone value
 //undefind
 //symbol= unquie
+let unquieSymbol= Symbol("111");
+let unquieSymbol1= Symbol("111");
+console.log(unquieSymbol)
 
 // Non Premitive type
 //object
 
 //console.log(typeof "Rajendra"); //String
 //console.log(typeof null); //object
-console.log(typeof undefined); //undefined
+//console.log(typeof undefined); //undefined
 
+// ----------------* Premitive Datatypes------------
+
+//                   Type                                   typeof
+
+// i)             Number                               number
+// ii)            String                                   string
+// iii)           Boolean                               boolean
+// iv)           Bigint                                   bigint
+// v)            Symbol                                symbol
+// vi)           Null                                      object
+// vii)          Undefined                           undefined
+
+
+// * -------Non-Premitive OR Referance OR Object datatype------
+
+//                   Type                                   typeof
+
+// i)               Object                                object
+// ii)              Array                                  object
+// iii)             Function                            function(object)
--- 01_basic/03_conversionOpration.js
+++ 01_basic/03_conversionOpration.js
@@ -1,8 +1,21 @@
 let score = "33abc";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let getIncrament=100;
+//console.log(getIncrament);
+// let value= getIncrament++
+// console.log(value);
+// console.log(getIncrament);
+
+/*
+JavaScript is a dynamically typed language. This means that:
+Type checking occurs at runtime:
+The type of a variable is determined by the value it holds at the time of execution, not during compilation.
+*/
+
