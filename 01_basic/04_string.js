f8ad828984aa0f4b3d56117009d149a06666d80f
Rajendra Singh Adhikari <60564843+rajendra1090@users.noreply.github.com>
2025-07-26 17:46:05 +0000

    string

---
+++ 01_basic/04_string.js
@@ -0,0 +1,7 @@
+let name='rajendra';
+let myAge=30;
+console.log(`My name is ${name} and age is ${myAge}`);
+console.log(name.toUpperCase());
+//String interpolation using backticks (``) and ${variable} syntax offers a more readable and efficient alternative to traditional concatenation methods.
+console.log(name.charAt(2));
+console.log(name.indexOf('r',0));
